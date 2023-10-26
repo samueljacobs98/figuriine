f6f82ed751d9cb7ca1f7fd3952cd44abcb8229dc
Samuel Jacobs <97236680+samueljacobs98@users.noreply.github.com>
2023-10-26 21:44:53 +0100

    Add Leva LUI and animate block (#3)
    * added GUI
    
    * style GUI and animate box

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,12 +1,41 @@
+import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+import { log } from "console";
+
 const Experience = () => {
+  const boxRef = useRef<THREE.Mesh | null>(null);
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+
+    if (boxRef.current) {
+      boxRef.current.rotation.y = Math.sin(a) * a * 0.005;
+    }
+  });
+
+  const { boxColour, position } = useControls({
+    boxColour: "#ff0000",
+    position: {
+      value: { x: 0, y: 0, z: 0 },
+      step: 0.1,
+    },
+  });
+
   return (
     <>
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh castShadow scale={1.5}>
+      <mesh
+        ref={boxRef}
+        castShadow
+        scale={1.5}
+        position={[position.x, position.y, position.z]}
+      >
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        <meshStandardMaterial color={boxColour} />
       </mesh>
 
       <mesh
