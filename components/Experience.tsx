659ab31a9ea1ded0972c760a15210197cf995f3b
Samuel Jacobs <sammyjacobscontact@gmail.com>
2023-10-27 16:35:48 +0100

    Imported first figure component and set up materials and debug

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,8 +1,19 @@
-import { useControls } from "leva";
+import { folder, useControls } from "leva";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
-import { log } from "console";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    hand: THREE.Mesh;
+    object: THREE.Mesh;
+  };
+  materials: {
+    HandsMaterial: THREE.MeshStandardMaterial;
+  };
+};
 
 const Experience = () => {
   const boxRef = useRef<THREE.Mesh | null>(null);
@@ -15,32 +26,53 @@ const Experience = () => {
     }
   });
 
-  const { boxColour, position } = useControls({
-    boxColour: "#ff0000",
-    position: {
-      value: { x: 0, y: 0, z: 0 },
-      step: 0.1,
-    },
+  const { headRotation, headColor, headPosition, headScale } = useControls({
+    modifiables: folder({
+      headColor: "#fcff00",
+    }),
+    debugHead: folder({
+      headRotation: {
+        value: { x: -Math.PI / 2, y: 0, z: 0 },
+        step: 0.1,
+      },
+      headPosition: {
+        value: { x: 0, y: 0, z: 0 },
+        step: 0.1,
+      },
+      headScale: 0.2,
+    }),
   });
+
+  const head = useGLTF("/head.glb") as GLTFResult;
 
   return (
     <>
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight
+        castShadow
+        position={[1, 2, 3]}
+        intensity={1.5}
+        shadow-normalBias={0.027}
+        shadow-bias={-0.004}
+      />
       <ambientLight intensity={0.5} />
 
-      <mesh
-        ref={boxRef}
-        castShadow
-        scale={1.5}
-        position={[position.x, position.y, position.z]}
+      <group
+        scale={0.3}
+        //  position={[position.x, position.y, position.z]}
+        //  rotation={[rotation.x, rotation.y - 0.75, rotation.z]}
       >
-        <boxGeometry />
-        <meshStandardMaterial color={boxColour} />
-      </mesh>
-
+        <mesh
+          geometry={head.nodes.object.geometry}
+          rotation={[headRotation.x, headRotation.y, headRotation.z]}
+          position={[headPosition.x, headPosition.y, headPosition.z]}
+          scale={headScale}
+        >
+          <meshStandardMaterial color={headColor} />
+        </mesh>
+      </group>
       <mesh
         receiveShadow
-        position-y={-1}
+        position-y={-0.9}
         rotation-x={-Math.PI * 0.5}
         scale={100}
       >
--- components/Renderer.tsx
+++ components/Renderer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Leva } from "leva";
@@ -7,25 +8,27 @@ import Experience from "./Experience";
 const Renderer = () => {
   return (
     <>
-      <div className="absolute right-0 pr-2 pt-2 z-10">
-        <Leva collapsed fill />
-      </div>
-      <Canvas
-        shadows
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 200,
-          position: [-4, 3, 6],
-        }}
-      >
-        <OrbitControls
-          makeDefault
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 2}
-        />
-        <Experience />
-      </Canvas>
+      <Suspense>
+        <div className="absolute right-0 pr-2 pt-2 z-10">
+          <Leva fill />
+        </div>
+        <Canvas
+          shadows
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [-4, 3, 6],
+          }}
+        >
+          <OrbitControls
+            makeDefault
+            minPolarAngle={0}
+            maxPolarAngle={Math.PI / 2}
+          />
+          <Experience />
+        </Canvas>
+      </Suspense>
     </>
   );
 };
