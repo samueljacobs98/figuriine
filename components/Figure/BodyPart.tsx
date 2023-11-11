603d1349e0a69a6bbe674fbfddd9744bb3f2a683
Samuel Jacobs <97236680+samueljacobs98@users.noreply.github.com>
2023-11-11 10:27:41 +0000

    Draco compression and Body Part Factory (#8)
    * Updated to use Draco models and refactored
    
    * Create BodyPartFactory

--- components/Figure/BodyPart.tsx
+++ components/Figure/BodyPart.tsx
@@ -1,32 +1,58 @@
-import * as THREE from "three";
+interface BodyPartData {
+  name: string;
+  geometry: THREE.BufferGeometry;
+  color: string;
+  offsetPosition: { x: number; y: number; z: number };
+  offsetRotation: { x: number; y: number; z: number };
+  scale: number;
+}
+
+class BodyPartFactory implements BodyPartData {
+  constructor(
+    public name: string,
+    public geometry: THREE.BufferGeometry,
+    public color: string,
+    public offsetPosition: { x: number; y: number; z: number } = {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    public offsetRotation: { x: number; y: number; z: number } = {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    public scale: number = 1
+  ) {}
+
+  render = () => <BodyPart data={this} />;
+}
 
 type BodyPartProps = {
-  name: string;
-  scale: number;
-  color: string;
-  geometry: THREE.BufferGeometry;
-  rotation: { x: number; y: number; z: number };
-  position: { x: number; y: number; z: number };
+  data: BodyPartData;
+  scale?: number;
 };
 
-const BodyPart = ({
-  scale,
-  color,
-  geometry,
-  rotation,
-  position,
-}: BodyPartProps) => {
+const BodyPart = ({ data }: BodyPartProps) => {
   return (
     <mesh
       castShadow
-      geometry={geometry}
-      rotation={[rotation.x, rotation.y, rotation.z]}
-      position={[position.x, position.y, position.z]}
-      scale={scale}
+      geometry={data.geometry}
+      rotation={[
+        data.offsetRotation.x,
+        data.offsetRotation.y,
+        data.offsetRotation.z,
+      ]}
+      position={[
+        data.offsetPosition.x,
+        data.offsetPosition.y,
+        data.offsetPosition.z,
+      ]}
+      scale={data.scale}
     >
-      <meshStandardMaterial color={color} />
+      <meshStandardMaterial color={data.color} />
     </mesh>
   );
 };
 
-export default BodyPart;
+export default BodyPartFactory;
